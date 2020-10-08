70dc9986b0eb0690bad11e63ddc9c46cae3ba6c3
anadunning <anakpedrosa@hotmail.com>
2020-10-07 22:57:30 -0300

    Read two integers and print the largest

--- 01.c
+++ 01.c
@@ -6,7 +6,7 @@ int main()
 {
     int a, b;
 
-    printf("Informe dois numeros inteiros quaisquer: \n");
+    printf("Inform two integer numbers: \n");
     scanf("%d %d", &a, &b);
 
     if (a > b)
